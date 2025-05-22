9d71a6fe0639b5b6cbff91d645ce53c759275348
Ignacio <ignacio.pulice@fairgate.io>
2025-05-22 18:34:42 -0300

    Refactor, adding a new constructor to allow optionals settings

--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,27 @@ use storage_backend::storage_config::StorageConfig;
 #[derive(Debug, Deserialize, Clone)]
 pub struct KeyManagerConfig {
     pub network: String,
-    pub key_derivation_seed: String,
-    pub key_derivation_path: String,
-    pub winternitz_seed: String,
+    pub key_derivation_seed: Option<String>,
+    pub key_derivation_path: Option<String>,
+    pub winternitz_seed: Option<String>,
 }
+
+impl KeyManagerConfig {
+    pub fn new(
+        network: String,
+        key_derivation_seed: Option<String>,
+        key_derivation_path: Option<String>,
+        winternitz_seed: Option<String>,
+    ) -> Self {
+        Self {
+            network,
+            key_derivation_seed,
+            key_derivation_path,
+            winternitz_seed,
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Clone)]
 #[serde(deny_unknown_fields)] // enforce strict field compliance
 pub struct Config {

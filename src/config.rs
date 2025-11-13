930c766032fc5fc6b9243d33f0eac91a1f4011b1
danielemiliogarcia <contacto@emiliogarcia.com.ar>
2025-11-13 19:05:29 -0300

    feat: settings mnemonic supports "" empty string

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,20 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use storage_backend::storage_config::StorageConfig;
+
+fn deserialize_optional_string<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let opt = Option::<String>::deserialize(deserializer)?;
+    Ok(opt.and_then(|s| if s.is_empty() { None } else { Some(s) }))
+}
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct KeyManagerConfig {
     pub network: String,
+    #[serde(default, deserialize_with = "deserialize_optional_string")]
     pub mnemonic_sentence: Option<String>,
+    #[serde(default, deserialize_with = "deserialize_optional_string")]
     pub mnemonic_passphrase: Option<String>,
 }
 

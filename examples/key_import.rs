b8043458bcd9de462d4d2052e50adc1b5517cc4a
danielemiliogarcia <contacto@emiliogarcia.com.ar>
2025-12-17 13:25:49 -0300

    refactor: add zeroize to secrets in examples

--- examples/key_import.rs
+++ examples/key_import.rs
@@ -2,33 +2,54 @@ mod create;
 
 use bitcoin::Network;
 use create::{create_key_manager_example, random_bytes};
+use key_manager::key_type::BitcoinKeyType;
 
 fn main() {
     // see function code, main is just a wrapper to run the example
     import_key_to_key_manager_example();
 }
 
-// TODO zeroize the caller os responsible for zeroizing the private keys after use?
-
 pub fn import_key_to_key_manager_example() {
     let network = Network::Regtest;
     let key_manager = create_key_manager_example("key_import");
 
     // --- Key importing
+    // Note that it is responsibility of the caller to zeroize private keys after use, and they in memory lifespan management
     use bitcoin::secp256k1::SecretKey;
     use bitcoin::PrivateKey;
-    let secret_key = SecretKey::from_slice(&random_bytes()).unwrap();
+    use zeroize::Zeroizing;
+
+    // -- Importing a single private key without typing it (internally its type 'unknown')
+    let random_bytes_z = Zeroizing::new(random_bytes()); // adds automatic zeroization on drop
+    let secret_key = SecretKey::from_slice(& *random_bytes_z).unwrap();
     let private_key = PrivateKey::new(secret_key, network);
+    let private_key_wif_z = Zeroizing::new(private_key.to_wif()); // adds automatic zeroization on drop
+
     let pubkey = key_manager
-        .import_private_key(&private_key.to_wif())
+        .import_private_key(& *private_key_wif_z)
         .unwrap();
     println!("Imported public key: {}", pubkey);
 
-    let secret_key2 = SecretKey::from_slice(&random_bytes()).unwrap();
+    // -- Importing a single private key with type specified
+    let random_bytes2_z = Zeroizing::new(random_bytes()); // adds automatic zeroization on drop
+    // In this case we are using random bytes for the sake of this example simplicity,
+    // but in real use cases the bytes should come from a secure source following BIP-39 derivation path for the desired key type
+    let secret_key2 = SecretKey::from_slice(& *random_bytes2_z).unwrap();
     let private_key2 = PrivateKey::new(secret_key2, network);
+    let private_key_wif2_z = Zeroizing::new(private_key2.to_wif()); // adds automatic zeroization on drop
 
+    // The key type could be any of the supported BitcoinKeyType variants (P2pkh, P2shP2wpkh, P2wpkh, P2tr)
+    let key2_type = BitcoinKeyType::P2tr;
+    let pubkey = key_manager
+        .import_private_key_typed(& *private_key_wif2_z, Some(key2_type))
+        .unwrap();
+    println!("Imported public key 2: {} of type {:?}", pubkey, key2_type);
+
+
+    // TODO zeroize
     let private_keys: Vec<String> =
-        vec![private_key.to_wif().clone(), private_key2.to_wif().clone()];
+        vec![(*private_key_wif_z).clone(), (*private_key_wif2_z).clone()];
+
     let pubkey = key_manager
         .import_partial_private_keys(private_keys, network)
         .unwrap();
@@ -41,6 +62,7 @@ pub fn import_key_to_key_manager_example() {
         secret_key.display_secret().to_string(),
         secret_key2.display_secret().to_string(),
     ];
+    // TODO zeroize
     let pubkey = key_manager
         .import_partial_secret_keys(secret_keys, network)
         .unwrap();

0b0d353d3593b9652db6eccbffeddc5af7a1e6da
Valentin Vaninetti <vaninettivalentin@gmail.com>
2026-03-04 19:06:54 -0300

    added requested fixes for both merged prs, suite5 tests and suite6 tests

--- src/tests/rsa_test.rs
+++ src/tests/rsa_test.rs
@@ -17,25 +17,25 @@ mod rsa_tests {
     #[test]
     fn test_sign_and_verify() {
         let key_manager = create_key_manager("test_output/rsa_sign_verify", None).unwrap();
-        let pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
+        let pubkey_pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
         let message = random_bytes();
-        let signature = key_manager.sign_rsa_message(&message, &pem).unwrap();
+        let signature = key_manager.sign_rsa_message(&message, &pubkey_pem).unwrap();
 
         let verifier = SignatureVerifier::new();
-        assert!(verifier.verify_rsa_signature(&signature, &message, &pem).unwrap());
+        assert!(verifier.verify_rsa_signature(&signature, &message, &pubkey_pem).unwrap());
         clear_output();
     }
 
     #[test]
     fn test_signature_rejects_tampered_message() {
         let key_manager = create_key_manager("test_output/rsa_tampered_msg", None).unwrap();
-        let pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
+        let pubkey_pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
         let mut message = random_bytes();
-        let signature = key_manager.sign_rsa_message(&message, &pem).unwrap();
+        let signature = key_manager.sign_rsa_message(&message, &pubkey_pem).unwrap();
 
         message[0] ^= 0xFF;
         let verifier = SignatureVerifier::new();
-        assert!(!verifier.verify_rsa_signature(&signature, &message, &pem).unwrap());
+        assert!(!verifier.verify_rsa_signature(&signature, &message, &pubkey_pem).unwrap());
         clear_output();
     }
 
@@ -43,24 +43,24 @@ mod rsa_tests {
     fn test_signature_does_not_verify_with_different_key() {
         let key_manager = create_key_manager("test_output/rsa_wrong_key", None).unwrap();
         let mut rng = thread_rng();
-        let pem_a = key_manager.generate_rsa_keypair(&mut rng).unwrap();
-        let pem_b = key_manager.generate_rsa_keypair(&mut rng).unwrap();
+        let pubkey_pem_a = key_manager.generate_rsa_keypair(&mut rng).unwrap();
+        let pubkey_pem_b = key_manager.generate_rsa_keypair(&mut rng).unwrap();
         let message = random_bytes();
-        let signature = key_manager.sign_rsa_message(&message, &pem_a).unwrap();
+        let signature = key_manager.sign_rsa_message(&message, &pubkey_pem_a).unwrap();
 
         let verifier = SignatureVerifier::new();
-        assert!(!verifier.verify_rsa_signature(&signature, &message, &pem_b).unwrap());
+        assert!(!verifier.verify_rsa_signature(&signature, &message, &pubkey_pem_b).unwrap());
         clear_output();
     }
 
     #[test]
     fn test_encrypt_decrypt_roundtrip() {
         let key_manager = create_key_manager("test_output/rsa_enc_dec", None).unwrap();
-        let pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
+        let pubkey_pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
         let message = random_bytes();
 
-        let encrypted = key_manager.encrypt_rsa_message(&message, &pem).unwrap();
-        let decrypted = key_manager.decrypt_rsa_message(&encrypted, &pem).unwrap();
+        let encrypted = key_manager.encrypt_rsa_message(&message, &pubkey_pem).unwrap();
+        let decrypted = key_manager.decrypt_rsa_message(&encrypted, &pubkey_pem).unwrap();
         assert_eq!(message, decrypted);
         clear_output();
     }
@@ -69,9 +69,9 @@ mod rsa_tests {
     fn test_sign_with_unknown_key_returns_not_found() {
         let key_manager = create_key_manager("test_output/rsa_unknown_sign", None).unwrap();
         let other = create_key_manager("test_output/rsa_unknown_sign_other", None).unwrap();
-        let foreign_pem = other.generate_rsa_keypair(&mut thread_rng()).unwrap();
+        let foreign_pubkey_pem = other.generate_rsa_keypair(&mut thread_rng()).unwrap();
 
-        let result = key_manager.sign_rsa_message(&random_bytes(), &foreign_pem);
+        let result = key_manager.sign_rsa_message(&random_bytes(), &foreign_pubkey_pem);
         assert!(matches!(result, Err(KeyManagerError::RsaKeyNotFound)));
         clear_output();
     }
@@ -79,15 +79,14 @@ mod rsa_tests {
     #[test]
     fn test_decrypt_with_unknown_key_returns_not_found() {
         let key_manager = create_key_manager("test_output/rsa_unknown_dec", None).unwrap();
-        let pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
-        let encrypted = key_manager.encrypt_rsa_message(&random_bytes(), &pem).unwrap();
+        let pubkey_pem = key_manager.generate_rsa_keypair(&mut thread_rng()).unwrap();
+        let encrypted = key_manager.encrypt_rsa_message(&random_bytes(), &pubkey_pem).unwrap();
 
         let other = create_key_manager("test_output/rsa_unknown_dec_other", None).unwrap();
-        let foreign_pem = other.generate_rsa_keypair(&mut thread_rng()).unwrap();
+        let foreign_pubkey_pem = other.generate_rsa_keypair(&mut thread_rng()).unwrap();
 
-        let result = key_manager.decrypt_rsa_message(&encrypted, &foreign_pem);
+        let result = key_manager.decrypt_rsa_message(&encrypted, &foreign_pubkey_pem);
         assert!(matches!(result, Err(KeyManagerError::RsaKeyNotFound)));
         clear_output();
     }
 }
-
